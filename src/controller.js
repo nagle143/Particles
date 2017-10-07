ba1b0f46ca4970c19e82b52341c72604efeea97c
nagleTroy <naglet@ksu.edu>
2017-10-07 18:11:10 -0500

    Collsions working

--- src/controller.js
+++ src/controller.js
@@ -2,6 +2,9 @@ import Particle from './particle.js';
 
 export default class Controller {
   constructor() {
+    this.numParticles = 25;
+    this.totalXspeed = 0;
+    this.totalYspeed = 0;
     this.particles = [];
     this.createParticles();
     //Back Buffer
@@ -23,24 +26,61 @@ export default class Controller {
     this.loop = this.loop.bind(this);
     //this.calcDistance = this.calcDistance.bind(this);
     this.createParticles = this.createParticles.bind(this);
+    this.rotate = this.rotate.bind(this);
+    this.particleCollision = this.particleCollision.bind(this);
+    this.handleKeyDown = this.handKeyDown.bind(this);
+    window.onkeydown = this.handleKeyDown;
 
     //Loop
     this.interval = setInterval(this.loop, 10);
   }
 
+  handKeyDown(event) {
+    event.preventDefault();
+    console.log(event.key);
+    switch (event.key) {
+      case 'i':
+      case 'I':
+        console.log(this.particles);
+        break;
+      case 's':
+        this.totalXspeed = 0;
+        this.totalYspeed = 0;
+        this.particles.forEach(particle => {
+          this.totalXspeed += particle.speed.x;
+          this.totalYspeed += particle.speed.y;
+        });
+        console.log(this.totalXspeed);
+        console.log(this.totalYspeed);
+        break;
+      case '+':
+        this.addParticle();
+        break;
+      case '-':
+        this.removeParticle();
+        break;
+    }
+  }
+
   createParticles() {
+    while(this.particles.length < this.numParticles) {
+      this.addParticle();
+    }
+  }
+
+  addParticle() {
     var x;
     var y;
     var radius;
     var mass;
     var id = 0;
-
-    while(this.particles.length <= 15) {
+    var currLength = this.particles.length;
+    while (currLength === this.particles.length) {
       var collision = false;
-      x = Math.floor(Math.random() * 900) + 50;
-      y = Math.floor(Math.random() * 900) + 50;
-      radius = Math.floor(Math.random() * 30 + 10);
-      mass = Math.floor(Math.random() * 10);
+      x = Math.random() * 900 + 50;
+      y = Math.random() * 900 + 50;
+      radius = Math.random() * 30 + 10;
+      mass = Math.floor(Math.random() * 10) + 1;
       if(this.particles.length === 0) {
         this.particles.push(new Particle(x, y, radius, mass, id));
         id++;
@@ -57,12 +97,74 @@ export default class Controller {
         }
       }
     }
-    console.log(this.particles);
+  }
+
+  removeParticle() {
+    this.particles.pop();
+  }
+
+  rotate(velocity, angle) {
+    const rotatedVelocities = {
+        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+    };
+    return rotatedVelocities;
+  }
+
+  particleCollision(particle, otherParticle) {
+    var xVelocityDiff = particle.speed.x - otherParticle.speed.x;
+    var yVelocityDiff = particle.speed.y - otherParticle.speed.y;
+
+    var xDist = otherParticle.x - particle.x;
+    var yDist = otherParticle.y - particle.y;
+
+    // Prevent accidental overlap of particles
+    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+
+        // Grab angle between the two colliding particles
+        var angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+
+        // Store mass in var for better readability in collision equation
+        var m1 = particle.mass;
+        var m2 = otherParticle.mass;
+
+        // Velocity before equation
+        var u1 = this.rotate(particle.speed, angle);
+        var u2 = this.rotate(otherParticle.speed, angle);
+
+        // Velocity after 1d collision equation
+        var v1 = { x: (u1.x * (m1 - m2) + 2 * m2 * u2.x) / (m1 + m2), y: u1.y };
+        var v2 = { x: (u2.x * (m2 - m1) + 2 * m1 * u1.x)/ (m1 + m2), y: u2.y };
+
+        // Final velocity after rotating axis back to original location
+        var vFinal1 = this.rotate(v1, -angle);
+        var vFinal2 = this.rotate(v2, -angle);
+
+        // Swap particle velocities for realistic bounce effect
+        //particle.updateSpeed(vFinal1.x, vFinal2.y);
+        //otherParticle.updateSpeed(vFinal2.x, vFinal2.y);
+        particle.speed.x = vFinal1.x;
+        particle.speed.y = vFinal1.y;
+        otherParticle.speed.x = vFinal2.x;
+        otherParticle.speed.y = vFinal2.y;
+    }
   }
 
   update() {
-
+    this.particles.forEach(particle => {
+      particle.update();
+    });
+    for(var i = 0; i < this.particles.length; i++) {
+      for(var j = 0; j < this.particles.length; j++) {
+        if(i != j) {
+          if(this.particles[i].collisionDetection(this.particles[j].x, this.particles[j].y, this.particles[j].radius)) {
+            this.particleCollision(this.particles[i], this.particles[j]);
+          }
+        }
+      }
+    }
   }
+
   render() {
     this.backBufferContext.fillStyle = 'black';
     this.backBufferContext.fillRect(0,0,1000,1000);
@@ -71,6 +173,7 @@ export default class Controller {
     });
     this.screenBufferContext.drawImage(this.backBufferCanvas, 0,0)
   }
+
   loop() {
     this.update();
     this.render();
--- src/particle.js
+++ src/particle.js
@@ -4,11 +4,15 @@ export default class Particle {
   constructor(x, y, radius, mass, id) {
     this.x = x;
     this.y = y;
-    this.radius = radius;
+    this.radius;
     this.mass = mass;
+    this.color;
+    this.fill;
+    this.initColorAndRadius();
     this.ID = id;
-    this.speedX = 0.0;
-    this.speedY = 0.0;
+    this.speed = {x: 0.0, y: 0.0};
+    this.MAXSPEED = {x: 20, y: 20};
+    this.initSpeed();
     this.velocity = {mag: 0.0, dir: 0.0};
     this.temp = {x: this.x, y: this.y, radius: this.radius};
     //console.log(this.temp);
@@ -19,6 +23,10 @@ export default class Particle {
     this.update = this.update.bind(this);
     this.render = this.render.bind(this);
     this.collisionDetection = this.collisionDetection.bind(this);
+    this.edgeDetection = this.edgeDetection.bind(this);
+    this.initSpeed = this.initSpeed.bind(this);
+    this.initColor = this.initColorAndRadius.bind(this);
+    this.updateSpeed = this.updateSpeed.bind(this);
   }
 
   getRadius() {
@@ -29,6 +37,62 @@ export default class Particle {
     return this.mass;
   }
 
+  initSpeed() {
+    this.speed.x = Math.random() * 8 - 4;
+    this.speed.y = Math.random() * 8 - 4;
+  }
+
+  updateSpeed(x, y) {
+    if(Math.abs(x) > this.MAXSPEED.x) {
+      this.speed.x = 20;
+    }
+    else {
+      this.speed.x = x;
+    }
+    if(Math.abs(y) > this.MAXSPEED.y) {
+      this.speed.s = 20;
+    }
+    else {
+      this.speed.y = y;
+    }
+  }
+
+  initColorAndRadius() {
+    switch (this.mass) {
+      case 1:
+      case 2:
+      case 3:
+        this.radius = 10;
+        this.color = 'red';
+        break;
+      case 4:
+        this.radius = 15;
+        this.color = 'cyan';
+        break;
+      case 5:
+        this.radius = 20;
+        this.color = 'green';
+        break;
+      case 6:
+        this.radius = 25;
+        this.color = 'blue';
+        break;
+      case 7:
+        this.radius = 30;
+        this.color = 'violet';
+        break;
+      case 8:
+      case 9:
+      case 10:
+        this.radius = 40;
+        this.color = "purple";
+        break;
+      default:
+        this.color = "yellow";
+        this.radius = 50;
+    }
+  }
+
   collisionDetection(cx, cy, cradius) {
     var distance = Math.pow(this.x - cx, 2) + Math.pow(this.y - cy, 2);
     if(distance < Math.pow(this.radius + cradius, 2)) {
@@ -37,19 +101,64 @@ export default class Particle {
     return false;
   }
 
-  update() {
+  edgeDetection() {
+    if(this.x + this.radius >= 1000 && this.speed.x > 0) {
+      this.speed.x = -this.speed.x;
+    }
+    if(this.x - this.radius <= 0 && this.speed.x < 0) {
+      this.speed.x = -this.speed.x;
+    }
+    if(this.y + this.radius >= 1000 && this.speed.y > 0) {
+      this.speed.y = -this.speed.y;
+    }
+    if(this.y - this.radius <= 0 && this.speed.y < 0) {
+      this.speed.y = -this.speed.y;
+    }
+  }
 
+  determineFill() {
+    if(this.velocity.mag <= 3) {
+      this.fill = 'blue';
+    }
+    else if(this.velocity.mag > 3 && this.velocity.mag <= 5) {
+      this.fill = 'orange';
+    }
+    else if(this.velocity.mag > 5 && this.velocity.mag <= 7) {
+      this.fill = 'yellow';
+    }
+    else if(this.velocity.mag > 9 && this.velocity.mag <= 15) {
+      this.fill = 'red';
+    }
+    else {
+      this.fill = 'white';
+    }
+  }
+
+  update() {
+    this.edgeDetection();
+    this.x += this.speed.x;
+    this.y += this.speed.y;
+    this.velocity.mag = Math.sqrt(this.speed.x * this.speed.x + this.speed.y * this.speed.y);
+    this.determineFill();
   }
 
   render(context) {
     context.save();
     context.font = "15px Times New Roman";
-    context.strokeStyle = 'red';
+    context.strokeStyle = this.fill;
+    context.fillStyle = this.color;
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     context.closePath();
     context.stroke();
-    context.strokeText(this.ID, this.x, this.y)
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.closePath();
+    context.save();
+    context.globalAlpha = 0.2;
+    context.fill();
+    context.restore();
+    //context.strokeText(this.ID, this.x, this.y)
     context.restore();
   }
 }
